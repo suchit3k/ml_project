73f214a848ce2bb05f85a1dd38bf1877edfa692c
suchit3k <suchitraj8@gmail.com>
2023-11-02 15:25:23 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+
+def get_requirements(file_path:str())->List[str]:
+    '''
+    this function return the list of requirement
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requiremnts = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+name = 'ml_project',
+version = '0.0.1',
+author = 'suchit',
+author_email = 'suchitraj8@gmail.com',
+packages = find_packages(),
+install_requires = get_requirements('requirements.txt')
+)
